86b3fe5c4cfd4affbbd65852c93ec4deb744e60e
Veld <velddev@gmail.com>
2018-06-13 04:09:49 +0200

    Add: initial

---
+++ Miki.Logging/Miki.Logging/LogLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miki.Logging
+{
+	public enum LogLevel
+	{
+		None,
+		Information,
+		Warning,
+		Error,
+		Debug,
+		Verbose
+	}
+}
